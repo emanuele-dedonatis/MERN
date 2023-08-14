dee4aa3f06ae9cab1709a2eb5ad736b1b420e3e7
Emanuele De Donatis <ede@loriot.io>
2023-08-14 17:48:50 +0200

    Add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,32 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { DashboardLayout, HomeLayout, Landing, Login, Register } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 export default App;
---
+++ src/pages/HomeLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+function HomeLayout() {
+  return (
+    <div>
+      <nav>navbar</nav>
+      <Outlet />
+    </div>
+  );
+}
+export default HomeLayout;
