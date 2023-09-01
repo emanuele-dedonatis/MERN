3d4f359a86e3a1e2836b195c21193010453822d3
Emanuele De Donatis <ede@loriot.io>
2023-09-01 14:58:46 +0200

    Dashboard context

--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -1,21 +1,51 @@
 import { Outlet } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
+import { createContext, useContext, useState } from 'react';
+
+const dashboardContext = createContext({});
 
 function DashboardLayout() {
+  const user = 'emanuele';
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const toggleDarkTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  const logoutUser = () => {
+    console.log('logout');
+  };
+
   return (
-    <Wrapper>
-      <main className='dashboard'>
-        <SmallSidebar />
-        <BigSidebar />
-        <div>
-          <Navbar />
-          <div className='dashboard-page'>
-            <Outlet />
+    <dashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+      }}
+    >
+      <Wrapper>
+        <main className='dashboard'>
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className='dashboard-page'>
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </main>
-    </Wrapper>
+        </main>
+      </Wrapper>
+    </dashboardContext.Provider>
   );
 }
+
+export const useDahsboardContext = () => useContext(dashboardContext);
 export default DashboardLayout;
