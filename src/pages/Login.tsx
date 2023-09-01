091c801c8acd74cc498125eea4160ebf4f4f6386
Emanuele De Donatis <ede@loriot.io>
2023-09-01 14:39:07 +0200

    Login Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,39 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 
 function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to='/register'>Register</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>login</h4>
+        <FormRow
+          type='email'
+          name='email'
+          defaultValue='emanuele.dedonatis@gmail.com'
+          required
+        />
+        <FormRow
+          type='password'
+          name='password'
+          defaultValue='secret123'
+          required
+        />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>
+            register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 }
 export default Login;
