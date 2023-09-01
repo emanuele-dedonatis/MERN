702ebd57ca4a546c5254f49a13acfe2d73fa9892
Emanuele De Donatis <ede@loriot.io>
2023-09-01 15:48:51 +0200

    Small sidebar links

--- src/components/SmallSidebar.tsx
+++ src/components/SmallSidebar.tsx
@@ -1,10 +1,37 @@
+import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { useDahsboardContext } from '../pages/DashboardLayout';
+import { Logo } from '.';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
 
 function SmallSidebar() {
   const data = useDahsboardContext();
-  console.log(data);
 
-  return <Wrapper>SmallSidebar</Wrapper>;
+  return (
+    <Wrapper>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button type='button' className='close-btn'>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map((link) => {
+              const { text, path, icon } = link;
+              return (
+                <NavLink to={path} key={text} className='nav-link'>
+                  <span className='icon'>{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 export default SmallSidebar;
