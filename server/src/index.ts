5c957e7c5f16acad100ae32d3ab2f73f163472e9
Emanuele De Donatis <ede@loriot.io>
2023-09-01 17:26:15 +0200

    Create job

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import morgan from 'morgan'
 import 'dotenv/config'
+import { randomUUID } from 'crypto';
 
 let jobs = [
- { id: 1, company: 'apple', position: 'front-end'},
- { id: 2, company: 'google', position: 'back-end'},
+ { id: randomUUID(), company: 'apple', position: 'front-end'},
+ { id: randomUUID(), company: 'google', position: 'back-end'},
 ]
 const app = express();
 
@@ -14,8 +15,25 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); 
 }
 
+// GET ALL JOBS
 app.get('/api/v1/jobs', (req, res) => {
-    res.status(200).json(jobs).send();
+    res.status(200).json({jobs}).send();
+})
+
+// CREATE JOB
+app.post('/api/v1/jobs', (req, res) => {
+    const {company, position} = req.body;
+    if (!company || !position) {
+        res.status(400).json({msg: 'please provide company and position'}).send();
+    } else {
+        const job = {
+            id: randomUUID(),
+            company,
+            position
+        }
+        jobs.push(job);
+        res.status(200).json(job).send();
+    }
 })
 
 const port = process.env.PORT || 5100
