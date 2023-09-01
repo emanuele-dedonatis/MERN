f3138a0db047d39c6448c36b5338366fd973933b
Emanuele De Donatis <ede@loriot.io>
2023-09-01 17:08:41 +0200

    Add morgan and dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,24 @@
 import express from 'express'
+import morgan from 'morgan'
+import 'dotenv/config'
+
+
 const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev')); 
+}
 
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
+app.post('/', (req, res) => {
+    res.json(req.body).send();
+})
 
-app.listen(5100, () => {
-    console.log('server running on port 5100')
+const port = process.env.PORT || 5100
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
 })
